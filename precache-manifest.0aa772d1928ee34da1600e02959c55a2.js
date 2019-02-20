ae2e648f7d41069349ccd27e79127a2994f05f01
AhmadG1 <ahmad.sr.yaseen@gmail.com>
2019-02-20 16:05:18 +0300

    Updates

---
+++ precache-manifest.0aa772d1928ee34da1600e02959c55a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "489f45e2a7e6f6c5dc6a",
+    "url": "/to-do-list-project-2/static/css/main.84f0c914.chunk.css"
+  },
+  {
+    "revision": "489f45e2a7e6f6c5dc6a",
+    "url": "/to-do-list-project-2/static/js/main.489f45e2.chunk.js"
+  },
+  {
+    "revision": "46bbd1d038687260f8d3",
+    "url": "/to-do-list-project-2/static/js/runtime~main.46bbd1d0.js"
+  },
+  {
+    "revision": "f1320c0db08a9d99840a",
+    "url": "/to-do-list-project-2/static/js/2.f1320c0d.chunk.js"
+  },
+  {
+    "revision": "37cef7670fee342b81fbb9974a5ef8da",
+    "url": "/to-do-list-project-2/index.html"
+  }
+];
